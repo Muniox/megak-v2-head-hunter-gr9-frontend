60bc570d3db2f6ca8ed103f3a28cfc9cf63b622d
MaJan433 <69705881+MaJan433@users.noreply.github.com>
2023-04-19 13:30:56 +0200

    change language to polish

--- src/components/RegistrationResultPage.tsx
+++ src/components/RegistrationResultPage.tsx
@@ -14,8 +14,8 @@ export const RegistrationResultPage = ({ isSuccessful, reason }: Props) => {
     <div className="flex justify-center items-center h-screen">
         <div className="w-500 h-500 flex flex-col justify-center items-center bg-black shadow-lg p-8">
             {isSuccessful ?
-                <h1 className="text-white text-5xl font-bold mb-10">Thank you for registration</h1> :
-                <h1 className="text-white text-5xl font-bold mb-10">Registration unsuccessful, please {reason}</h1>
+                <h1 className="text-white text-5xl font-bold mb-10"> Dziękujemy za rejestrację </h1> :
+                <h1 className="text-white text-5xl font-bold mb-10"> Rejestracja nieudana z powodu {reason} </h1>
             }
             <button onClick={handleReturn} type="button" className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4">
                 Return
