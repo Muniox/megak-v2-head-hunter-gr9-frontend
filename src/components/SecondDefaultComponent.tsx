4c4f3631d0d534470e7dbe59068d718fc14c43ed
Muniox <muniox20@gmail.com>
2023-04-23 17:05:51 +0200

    HH44: Deleted unnecessary components

--- src/components/SecondDefaultComponent.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { DefaultComponent } from './DefaultComponent';
-
-
-export const SecondDefaultComponent = () => <DefaultComponent email="test@email.com" firstName='test Name' roleType='test'/>
