f227490d8bb433fadb378585a3d9521b26ccde22
Muniox <muniox20@gmail.com>
2023-04-26 23:01:47 +0200

    Changed eslint and vite configuration with formated Components by eslint.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
 import React, { FC } from 'react';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import {Home} from "./components/views/Home/Home";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Home } from './components/views/Home/Home';
 
-export const App: FC = () => {
-
-return (
-    <BrowserRouter>
-
-        <Routes>
-            <Route path='/' element={<Home/>}/>
-        </Routes>
-    </BrowserRouter>
+export const App: FC = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  </BrowserRouter>
 );
-}
-
-
-
--- src/components/views/Home/Home.tsx
+++ src/components/views/Home/Home.tsx
@@ -1,9 +1,3 @@
 import React from 'react';
 
-export const Home = () => {
-    return (
-
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
-
-    )
-}
+export const Home = () => <h1 className="text-3xl font-bold underline">Hello world!</h1>;
