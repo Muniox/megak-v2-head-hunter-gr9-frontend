2425b51416e95cf260a41cd1dcb7641679a25ebc
Muniox <muniox20@gmail.com>
2023-04-27 00:29:21 +0200

    Fixed problems with image and updated branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import React, {FC} from 'react';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import React, { FC } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import {LoginPage} from "./views/LoginPage";
-import {SampleDashboard} from "./views/SampleDashboard";
+import { LoginPage } from './views/LoginPage';
+import { SampleDashboard } from './views/SampleDashboard';
 
 export const App: FC = () => (
   <div>
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={< LoginPage />}/>
-        <Route path="/dashboard" element={< SampleDashboard />}/>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/dashboard" element={<SampleDashboard />} />
       </Routes>
     </BrowserRouter>
   </div>
