2a0853a54d67abdf2c9557818c5e5a66b3330c23
MaJan433 <janiszkiewicz.maciej@gmail.com>
2023-04-26 13:01:54 +0200

    Add RegistrationLanding component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React, { FC } from 'react';
 import { Logo } from './assets';
+import { RegistrationLandingPage } from './components/RegistrationLandingPage';
 
 export const App: FC = () => (
   <h1>
     Hello Group 9 ❤️
     <img src={Logo} alt="BackArrow" />
+    <RegistrationLandingPage />
   </h1>
 );
