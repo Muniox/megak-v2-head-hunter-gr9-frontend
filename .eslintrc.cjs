d2b218975f8b304d09cd6017e5b2b5f39584a443
Muniox <muniox20@gmail.com>
2023-04-26 23:28:57 +0200

    Changed eslint and vite configuration with formated Components by eslint.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,52 +1,52 @@
 module.exports = {
-  "env": {
-    "browser": true
+  env: {
+    browser: true,
   },
-  "extends": [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:import/recommended",
-    "plugin:jsx-a11y/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "airbnb",
-    "airbnb/hooks",
-    "eslint-config-prettier"
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:import/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'eslint-config-prettier',
   ],
-  "settings": {
-    "react": {
-      "version": "detect"
+  settings: {
+    react: {
+      version: 'detect',
     },
-    "import/resolver": {
-      "typescript": {
-        "alwaysTryTypes": true,
-        "project": ["./tsconfig.json"]
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+        project: ['./tsconfig.json'],
       },
-      "node": {
-        "paths": ["src"],
-        "extensions": [".js", ".jsx", ".ts", ".tsx"]
-      }
-    }
+      node: {
+        paths: ['src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
   },
-  "rules": {
-    "linebreak-style": ["error", "unix"],
-    "react/function-component-definition": [
+  rules: {
+    'linebreak-style': ['error', 'unix'],
+    'react/function-component-definition': [
       2,
       {
-        "namedComponents": "arrow-function",
-        "unnamedComponents": "arrow-function"
-      }
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
     ],
-    "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": "error",
-    "react/jsx-filename-extension": [1, { "extensions": [".ts", ".tsx"] }],
-    "import/no-extraneous-dependencies": "off",
-    "import/prefer-default-export": [0],
-    "import/extensions": [0],
-    "import/no-unresolved": [
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+    'react/jsx-filename-extension': [1, { extensions: ['.ts', '.tsx'] }],
+    'import/no-extraneous-dependencies': 'off',
+    'import/prefer-default-export': [0],
+    'import/extensions': [0],
+    'import/no-unresolved': [
       2,
       {
-        "ignore": ["^types$"]
-      }
-    ]
-  }
-}
+        ignore: ['^types$'],
+      },
+    ],
+  },
+};
