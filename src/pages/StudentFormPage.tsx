4e6816527428692ea18e4acb2e9b74fd7c6dee6f
Muniox <muniox20@gmail.com>
2023-05-08 09:58:32 +0200

    Fixed min height for form page

--- src/pages/StudentFormPage.tsx
+++ src/pages/StudentFormPage.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { StudentForm } from '../components/StudentForm/StudentForm';
 
 export const StudentFormPage = () => (
-  <main className="w-full h-screen bg-primary-color text-primary-font-color">
-    <div className="flex flex-col items-center w-3/4 h-screen mx-auto">
+  <main className="w-full min-h-screen bg-primary-color text-primary-font-color">
+    <div className="flex flex-col items-center w-3/4 mx-auto">
       <h1>Uzupełnij informacje o swoim profilu</h1>
       <StudentForm />
     </div>
