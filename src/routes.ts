1d12747a3eba5399e0e89ae978e71527e4e3114b
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-05-09 20:21:42 +0200

    HH83: add routes

---
+++ src/routes.ts
@@ -0,0 +1,7 @@
+export const routes = {
+  home: '/',
+  login: '/login',
+  register: '/register',
+  dashboard: '/dashboard',
+  forgetPassword: '/forget-password',
+};
--- src/routes/Routes.ts
+++
@@ -1,5 +0,0 @@
-export const routes = {
-  login: '/login',
-  dashboard: '/dashboard',
-  register: '/register',
-};
