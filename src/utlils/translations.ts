c8afd42f5956b92678ee548c768491715b76b6d5
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-05-09 20:19:53 +0200

    HH83: add translations file

---
+++ src/utlils/translations.ts
@@ -0,0 +1,18 @@
+export const translations = {
+  loginPage: {
+    forgotPassword: 'Zapomniałeś hasła?',
+    noAccount: 'Nie masz konta?',
+    register: 'Zarejestruj się',
+    login: 'Zaloguj się',
+  },
+  loginSchema: {
+    email: {
+      required: 'Email jest wymagany',
+      normal: 'Podaj prawidłowy adres e-mail',
+    },
+    password: {
+      required: 'Hasło jest wymagane',
+      min: 'Hasło musi mieć co najmniej 8 znaków',
+    },
+  },
+};
