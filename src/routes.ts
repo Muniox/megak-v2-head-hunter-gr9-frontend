269effff617cfaa96f04fb3fd453cacba48c7d30
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-05-10 18:08:54 +0200

    HH83: add RequireAuth, update routes

--- src/routes.ts
+++
@@ -1,7 +0,0 @@
-export const routes = {
-  home: '/',
-  login: '/login',
-  register: '/register',
-  dashboard: '/dashboard',
-  forgetPassword: '/forget-password',
-};
---
+++ src/routes/routes.ts
@@ -0,0 +1,12 @@
+export const routes = {
+  home: '/',
+  login: '/login',
+  registration: '/registration',
+  dashboard: '/dashboard',
+  forgetPassword: '/forget-password',
+  logout: '/logout',
+  addStudent: '/dashboard/add-student',
+  addHr: '/dashboard/add-hr',
+  availableStudents: '/dashboard/available-students',
+  toTalk: '/dashboard/to-talk',
+};
