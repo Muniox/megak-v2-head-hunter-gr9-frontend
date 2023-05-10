8dd2aa36440599314833fc89a06881788c88449e
Muniox <muniox20@gmail.com>
2023-05-10 11:26:21 +0200

    Added fetch to /api/students/profile

--- src/components/StudentForm/StudentForm.tsx
+++ src/components/StudentForm/StudentForm.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { StudentProfile } from './types/student-profile.request';
+import { ClientApiUserResponse } from '@frontendTypes';
+import { useNavigate } from 'react-router-dom';
+import { StudentProfileRequest } from '@backendTypes';
 import { useFormCustom } from './hooks/useFormCustom';
 import { useFieldProjectUrls } from './hooks/useFieldProjectUrls';
 import { useFieldPortfolioUrls } from './hooks/useFieldPortfolioUrls';
@@ -21,14 +23,43 @@ import {
   TargetWorkCityController,
   WorkExperienceController,
 } from './Forms';
+import { StudentProfile } from './types/student-profile.request';
 
 export const StudentForm = () => {
   const { control, handleSubmit, errors } = useFormCustom();
   const { projectUrlsFields, projectUrlsAppend, projectUrlsRemove } = useFieldProjectUrls(control);
   const { portfolioUrlsFields, portfolioUrlsAppend, portfolioUrlsRemove } = useFieldPortfolioUrls(control);
+  const navigate = useNavigate();
 
-  const onSubmit = (formValues: StudentProfile) => {
-    console.log('form data is', formValues);
+  const onSubmit = async (inputs: StudentProfile) => {
+    const portfolioUrls: string[] = [];
+    inputs.projectUrls.map((item) => portfolioUrls.push(item.url));
+
+    const projectUrls: string[] = [];
+    inputs.projectUrls.map((item) => projectUrls.push(item.url));
+
+    const newInputs: StudentProfileRequest = {
+      ...inputs,
+      portfolioUrls,
+      projectUrls,
+    };
+
+    const data = await fetch('http://localhost:3001/api/students/profile', {
+      method: 'POST',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newInputs),
+    });
+    const response = (await data.json()) as ClientApiUserResponse;
+    if (response.ok) {
+      navigate('/');
+    } else {
+      throw new Error(
+        Array.isArray(response.error) ? response.error[0].message : response.error?.message || 'Unknown error',
+      );
+    }
   };
 
   return (
