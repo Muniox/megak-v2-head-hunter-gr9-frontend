3240a786f012c0fc194dc88d6528abd890f11f59
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-05-10 18:05:49 +0200

    HH83: add useAuth hook

--- src/context/authContext.tsx
+++ src/context/authContext.tsx
@@ -1,8 +1,8 @@
-import React, { createContext, useEffect, useState } from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
 import { AuthContextProps, AuthContextProviderProps } from '@frontendTypes';
 
 import { LoginRequest, UserResponse } from '@backendTypes';
-import { apiClient, ENDPOINTS } from '../services';
+import { apiServer, ENDPOINTS } from '../services';
 
 export const AuthContext = createContext<AuthContextProps | null>(null);
 
@@ -12,7 +12,7 @@ export const AuthContextProvider = ({ children }: AuthContextProviderProps) => {
   );
 
   const login = async (credentials: LoginRequest) => {
-    const response = await apiClient.post<LoginRequest, UserResponse>(ENDPOINTS.login, credentials);
+    const response = await apiServer.post<LoginRequest, UserResponse>(ENDPOINTS.login, credentials);
 
     if (response.ok && response.data) {
       setCurrentUser(response.data);
@@ -28,4 +28,14 @@ export const AuthContextProvider = ({ children }: AuthContextProviderProps) => {
   }, [currentUser]);
 
   return <AuthContext.Provider value={{ currentUser, login }}>{children}</AuthContext.Provider>;
+};
+
+export const useAuth = (): AuthContextProps => {
+  const auth = useContext(AuthContext);
+
+  if (!auth) {
+    throw new Error('useAuth must be used within an AuthContextProvider.');
+  }
+
+  return auth;
 };
