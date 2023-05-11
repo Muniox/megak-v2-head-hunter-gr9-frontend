15759f1da1a1dfc9c677bfdef4a44ad6c392cfa3
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-05-11 15:04:23 +0200

    HH83: update AdHr

--- src/utlils/translations.ts
+++ src/utlils/translations.ts
@@ -8,7 +8,7 @@ export const translations = {
   authSchema: {
     email: {
       required: 'Email jest wymagany',
-      normal: 'Podaj prawidłowy adres e-mail',
+      normal: 'Email musi mieć odpowiedni format',
     },
     password: {
       required: 'Hasło jest wymagane',
@@ -23,9 +23,49 @@ export const translations = {
     },
   },
   navbar: {
-    addStudent: 'Dodaj Kursantów',
-    addHr: 'Dodaj HR',
+    addStudents: 'Dodaj Kursantów',
+    addHr: 'Dodaj Rekrutera',
     availableStudents: 'Dostępni Kursanci',
     toTalk: 'Do rozmowy',
+  },
+  addStudents: {
+    title: 'Dodaj Kursantów',
+    buttons: {
+      add: 'Prześlij plik',
+      chooseFile: 'Wybierz plik',
+    },
+  },
+  addHr: {
+    title: 'Dodaj Rekrutera',
+    forms: {
+      placeholders: {
+        email: 'Podaj email',
+        fullName: 'Podaj imię i nazwisko',
+        company: 'Podaj nazwę firmy',
+        maxReservedStudents: 'Podaj maksymalną ilość kursantów',
+      },
+      schema: {
+        email: {
+          required: 'Email jest wymagany',
+          normal: 'Email musi mieć odpowiedni format',
+        },
+        fullName: {
+          required: 'Imię jest wymagane',
+          min: 'Imię musi mieć co najmniej 3 znaki',
+          max: 'Imię może mieć maksymalnie 150 znaków',
+        },
+        company: {
+          required: 'Nazwa firmy jest wymagana',
+          min: 'Nazwa firmy musi mieć co najmniej 8 znaków',
+          max: 'Nazwa firmy może mieć maksymalnie 150 znaków',
+        },
+        maxReservedStudents: {
+          required: 'Wymagane jest podanie minimalnej ilości studentów do rekrutacji',
+          min: 'Minimalna ilość studentów to 1',
+          max: 'Maksymalna ilość studentów to 999',
+        },
+      },
+      submit: 'Dodaj',
+    },
   },
 };
