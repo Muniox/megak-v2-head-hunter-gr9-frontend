d2f01247fcbbef72e6b2a623d26d50173558ccf0
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-05-15 17:58:52 +0200

    HH57: remove components

--- src/views/ProfileHr/components/AvailableStudents.tsx
+++ src/views/ProfileHr/components/AvailableStudents.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import { StudentsTable } from '../../../components/StudentsTable/StudentsTable';
+import { mockStudentsList } from '../../../components/Student.type';
 
 export const AvailableStudents = () => {
-  return <div className="text-amber-300">Dostępni Studenci</div>;
+  return (
+    <div className="flex flex-col items-center justify-between w-full bg-secondary-color">
+      <div className="w-full  px-4 pt-4">
+        <div className="border border-available-students-details-divider-line-color">
+          <StudentsTable list={mockStudentsList} />;
+        </div>
+      </div>
+    </div>
+  );
 };
